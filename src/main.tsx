d2be9fd125bae6523e20a3ac5e349af4c4563cc4
Marlon Marcello <marlon.marcello@gmail.com>
2023-11-22 10:47:54 -0800

    wip: changed variable name to be inline with hook

--- src/main.tsx
+++ src/main.tsx
@@ -15,9 +15,10 @@ function CustomModal() {
     transitionDuration: TRANSITION,
   })
 
-  // this could be set in the stylesheet as well but for sake of example we'll do it here
+  // It's mandatory to be passed to the <Modal> component though either as inline styles or as a className
+  // for sake of example we'll do it here
   const stylesVars = {
-    "--duration": `${TRANSITION}ms`,
+    "--transition-duration": `${TRANSITION}ms`,
   } as React.CSSProperties
 
   return (
@@ -28,19 +29,19 @@ function CustomModal() {
       </button>
 
       {isOpen && (
-        <Modal
-          state={state}
-          className={classnames([
-            styles.CustomModal,
-            // you can also add separate classes for each state to customize the animation even further
-            (state === ModalStates.OPENING || state === ModalStates.OPEN) &&
-              styles.CustomModalOpen,
-            state === ModalStates.CLOSING && styles.CustomModalClosing,
-          ])}
-          style={stylesVars}
-        >
+        <Modal state={state} style={stylesVars}>
           <ModalBackdrop onClick={toggle} className={styles.CustomModalOverlay} />
-          <ModalContent className={styles.CustomModalContent}>
+          <ModalContent
+            className={classnames([
+              styles.CustomModalContent,
+              // up to you how you want to handle the animation
+              // you can also add separate classes for each state to customize the animation even further
+              // you can also simply customize the parent <Modal> component
+              // etc...
+              (state === ModalStates.OPENING || state === ModalStates.OPEN) &&
+                styles.CustomModalContentOpen,
+            ])}
+          >
             <button onClick={toggle} className={styles.CustomModalClose}>
               Close
             </button>
